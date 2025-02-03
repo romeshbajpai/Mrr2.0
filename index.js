7072f23ece02b68adaecaef644185b97e702bbed
RomeshBajpai12 <romesh.b@theaquarious.com>
2025-02-03 12:58:02 +0530

    middleware implimented

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const connectDB = require('./src/configs/db');
 const cors = require('cors');
+const userRoute = require('./src/routes/user.route')
 const app = express();
 require('dotenv').config();
 app.use(express.json());
+app.use('/api/v1/medi_reporting/user', userRoute);
 const port = process.env.PORT || 8081;
 app.listen(port, async () => {
     try {
